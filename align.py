e560afce7f324ec0fbc4a8bb57ec7bc44d25a843
James McFeeters <jmcfeeters@scripps.edu>
2026-01-15 14:34:39 -0800

    Align to ref in align.py
    Change mafft command to align to ref instead of pure MSA.
    Output both full alignment with ref and version with ref removed.

--- align.py
+++ align.py
@@ -8,15 +8,17 @@ import glob
 import os
 
 # inputs
-sras_file="data/sras.txt"
-fastas_dir='/Users/james/Documents/avian-influenza/fasta'
+sras_file = "data/sras.txt"
+fastas_dir = '/Users/james/Documents/avian-influenza/fasta'
 ambiguous_bases_frac_limit = 0.01
+ref_fasta = 'data/HA_reference.fasta'
 
 # outputs todo: remove test
 out_dir = 'out'
-concatenated_fasta = f"{out_dir}/concat.fa"
+concatenated_fasta = f"{out_dir}/concat.fasta"
 sras_qc_passed_file = f'{out_dir}/sras_qc_passed.txt'
-aligned_fasta = f'{out_dir}/aligned.fa'
+aligned_fasta = f'{out_dir}/ref_aligned.fasta'
+aligned_ref_removed_fasta = f'{out_dir}/ref_aligned_samples_only.fasta'
 
 Path(out_dir).mkdir(exist_ok=True)
 
@@ -42,7 +44,8 @@ seq_by_sra = dict()
 for sra, fasta in fasta_by_sra.items():
     with open(fasta, 'r') as f:
         contents = [s for s in SeqIO.parse(f, 'fasta')]
-        assert len(contents) == 1
+        assert len(contents) == 1 # check that file contains only one sequence
+        assert contents[0].id.find(sra) >= 0 # check that file name and contents match
         contents[0].name = sra
         contents[0].id = sra
         contents[0].description = ''
@@ -74,8 +77,23 @@ with open(concatenated_fasta, 'w+') as f:
 mafft_command = [
     'mafft',
     '--auto',
-    concatenated_fasta
+    '--keeplength',
+    '--addfragments',
+    concatenated_fasta,
+    ref_fasta
 ]
-
 with open(aligned_fasta, 'w+') as f:
     subprocess.run(mafft_command, stdout=f)
+
+# write a version of the MSA with the ref removed
+with open(ref_fasta, 'r') as f:
+    contents = [s for s in SeqIO.parse(f, 'fasta')]
+    assert len(contents) == 1
+    ref_seq = contents[0]
+
+with open(aligned_fasta, 'r') as f:
+    contents = [s for s in SeqIO.parse(f, 'fasta')]
+    aligned_ref_removed = [s for s in contents if s.id != ref_seq.id]
+
+with open(aligned_ref_removed_fasta, 'w+') as f:
+    SeqIO.write(aligned_ref_removed, f, 'fasta')
