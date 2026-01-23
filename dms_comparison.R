1d4fd155d2cf5459e9684ed73e2f466c81ecf40b
James McFeeters <jmcfeeters@scripps.edu>
2026-01-23 10:15:10 -0800

    Older changes to r scripts

--- dms_comparison.R
+++ dms_comparison.R
@@ -3,21 +3,39 @@ library(ggplot2)
 library(ggrepel)
 
 theme_set(theme_bw())
-setwd('/Users/james/Documents/convergent/out')
+setwd('/Users/james/Documents/convergent')
 
-mut_counts = read.csv('dn_ds_by_site_bovine_ha.tsv', sep='\t')
+mut_counts = read.csv('out/dn_ds_by_site_bovine_ha.tsv', sep='\t') %>% 
+  mutate(host='bovine')
 
-codons = read.csv('codon_mutation_counts_bovine_ha.tsv', sep='\t')
+mut_counts = full_join(
+  mut_counts,
+  read.csv('out_avian_b3.13/dn_ds_by_site_ha.tsv', sep='\t') %>% 
+    mutate(host='avian')
+)
+
+mut_counts = mut_counts %>% 
+  mutate(
+    ratio = case_when(
+      count_s == 0 ~ count_n,
+      .default = count_n / count_s
+    ),
+    zero_synonymous = (count_s == 0)
+  )
+
+codons = read.csv('out/codon_mutation_counts_bovine_ha.tsv', sep='\t') %>% 
+  mutate(host='bovine')
+codons = full_join(
+  codons,
+  read.csv('out_avian_b3.13/codon_mutation_counts_ha.tsv', sep='\t') %>% 
+    mutate(host='avian')
+)
 
 aminos = codons %>% 
-  group_by(pos_aa, ref_aa, alt_aa) %>% 
+  group_by(pos_aa, ref_aa, alt_aa, host) %>% 
   summarize()
-  # mutate(impact = case_when(
-  #   ref_aa == alt_aa ~ 'synonymous',
-  #   .default = 'non-synonymous'
-  # ))
 
-dms = read.csv('../data/dms_data_ha.tsv', sep='\t')
+dms = read.csv('data/dms_data_ha.tsv', sep='\t')
 dms = dms %>% 
   rename(
     pos_aa = position_aa,
@@ -29,15 +47,22 @@ dms = dms %>%
 
 aminos_dms = left_join(aminos, dms, by=c('pos_aa', 'ref_aa', 'alt_aa'))
 
+# *********************************************
 # what frac of amino mutations have dms values?
-count_uq_pos = aminos_dms$pos_aa %>% unique() %>% length()
+# *********************************************
+count_uq_pos = aminos_dms %>% 
+  distinct(pos_aa, host) %>% 
+  group_by(host) %>% 
+  summarize(n_uq_pos=n())
+  
 aminos_dms %>%
   ungroup() %>%
-  select(c('dms_name', 'pos_aa')) %>%
-  group_by(dms_name, pos_aa) %>% 
-  group_by(dms_name) %>%
-  summarize(n = n()) %>% 
-  mutate(frac_positions = n / count_uq_pos) %>% 
+  select(c('dms_name', 'pos_aa', 'host')) %>%
+  group_by(dms_name, pos_aa, host) %>%
+  group_by(dms_name, host) %>%
+  summarize(n = n()) %>%
+  left_join(count_uq_pos, by='host') %>% 
+  mutate(frac_positions = n / n_uq_pos ) %>%
   print()
 
 aminos_dms = aminos_dms %>% 
@@ -55,10 +80,10 @@ dn_ds_dms = mut_counts %>%
   filter(!dms_name %in% c('evescape_sigmoid', 'evescape'))
 
 p = ggplot(dn_ds_dms, aes(x=mean_dms_value, y=ratio)) +
-  geom_point() +
-  ggtitle('HA, bovine B3.13') +
-  ylab('dn/(ds+1)') +
-  facet_wrap(~dms_name, scales='free', ncol=4) +
+  geom_point(aes(shape=zero_synonymous)) +
+  ggtitle('HA, B3.13') +
+  ylab('dn/ds') +
+  facet_wrap(vars(dms_name, host), scales='free_x', ncol=4) +
   geom_text_repel(aes(label=pos_aa), max.overlaps=5, size=4)
 print(p)
 
--- dn_ds_plot.R
+++ dn_ds_plot.R
@@ -3,15 +3,31 @@ library(ggplot2)
 library(ggrepel)
 
 theme_set(theme_bw())
-setwd('/Users/james/Documents/convergent/out')
+setwd('/Users/james/Documents/convergent')
 
-mut_counts = read.csv('dn_ds_by_site_bovine_ha.tsv', sep='\t')
+mut_counts_bovine = read.csv('out/dn_ds_by_site_bovine_ha.tsv', sep='\t')
+mut_counts_avian = read.csv('out_avian_b3.13/dn_ds_by_site_ha.tsv', sep='\t')
+
+mut_counts = full_join(
+  mut_counts_bovine %>% mutate(host='bovine'),
+  mut_counts_avian %>% mutate(host='avian')
+)
+
+mut_counts = mut_counts %>% 
+  mutate(
+    ratio = case_when(
+      count_s == 0 ~ count_n,
+      .default = count_n / count_s
+    ),
+    zero_synonymous = (count_s == 0)
+  )
 
 p = ggplot(mut_counts, aes(x=pos_aa, y=ratio)) +
-  geom_point() +
+  geom_point(aes(color=zero_synonymous)) +
+  facet_wrap(~host, ncol=1) +
   geom_text_repel(aes(label=ifelse(ratio>2.5,pos_aa,''))) +
-  ggtitle('Selection in HA, bovine B3.13') +
+  ggtitle('Selection in HA, B3.13') +
   xlab('amino acid position') +
-  ylab('dn/(ds+1)')
+  ylab('dn/ds')
 print(p)
 
