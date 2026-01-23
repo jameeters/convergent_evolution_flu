5e5428c9c90e191dd5a4bd25770464f0186ba48f
James McFeeters <jmcfeeters@scripps.edu>
2026-01-23 11:04:42 -0800

    Add pipeline script for bovine PB2

--- pipeline_bovine_PB2.bash
+++ pipeline_bovine_PB2.bash
@@ -1,18 +1,20 @@
 #! /bin/bash
 
-OUT_DIR='out'
+set -euxo pipefail
 
-SRAS_FILE="data/sras.txt"
+OUT_DIR='out_bovine_b3.13_pb2'
+
+SRAS_FILE="data/sras_bovine_b3.13.txt"
 FASTAS_DIR="/Users/james/Documents/avian-influenza/fasta"
-REF_FASTA="data/HA_reference.fasta"
-REGION_NAME="HA"
+REF_FASTA="data/PB2_reference.fasta"
+REGION_NAME="PB2"
 
 CONCATENATED_FASTA="$OUT_DIR/concat.fasta"
 SRAS_QC_PASSED_FILE="$OUT_DIR/sras_qc_passed.txt"
 ALIGNED_WITH_REF_FASTA="$OUT_DIR/ref_aligned.fasta"
 ALIGNED_SAMPLES_ONLY_FASTA="$OUT_DIR/ref_aligned_samples_only.fasta"
 
-echo "align.py ..."
+
 python3 align.py \
 "$SRAS_FILE" \
 "$FASTAS_DIR" \
@@ -23,21 +25,19 @@ python3 align.py \
 
 SAMPLE_METADATA_FILE="$OUT_DIR/sample_metadata.tsv"
 
-echo "get_sample_metadata.py ..."
+
 python3 get_sample_metadata.py \
 "$SRAS_FILE" \
 "$SAMPLE_METADATA_FILE"
 
 GENOMIC_TREE_OUTPUT_PREFIX="$OUT_DIR/genomic_tree"
 
-echo "genomic_tree.bash ..."
 ./genomic_tree.bash \
 "$ALIGNED_SAMPLES_ONLY_FASTA" \
 "$GENOMIC_TREE_OUTPUT_PREFIX"
 
 TREETIME_OUT_DIR="$OUT_DIR/treetime"
 
-echo "time_tree.bash ..."
 ./time_tree.bash \
 "$GENOMIC_TREE_OUTPUT_PREFIX.treefile" \
 "$ALIGNED_SAMPLES_ONLY_FASTA" \
@@ -46,24 +46,21 @@ echo "time_tree.bash ..."
 
 ANCESTRAL_OUT_DIR="$OUT_DIR/ancestral"
 
-echo "ancestral.bash ..."
 ./ancestral.bash \
 "$TREETIME_OUT_DIR/timetree.nexus" \
 "$ALIGNED_SAMPLES_ONLY_FASTA" \
 "$ANCESTRAL_OUT_DIR"
 
 
-HA_GFF_FILE="data/HA.gff3"
+HA_GFF_FILE="data/PB2.gff3"
 TRANSLATION_OUT_DIR="$OUT_DIR/translation"
 
-echo "translate.py ..."
 python3 translate.py \
 "$HA_GFF_FILE" \
 "$REF_FASTA" \
 "$ANCESTRAL_OUT_DIR/ancestral_sequences.fasta" \
 "$TRANSLATION_OUT_DIR"
 
-echo "read_tree.py ..."
 python3 read_tree.py \
 "$ANCESTRAL_OUT_DIR" \
 "$TRANSLATION_OUT_DIR" \
